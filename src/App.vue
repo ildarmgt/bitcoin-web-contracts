5e79ada651d7adb005fd4ab00333cb2f6de2d611
ildarmgt <ildarmgt@gmail.com>
2019-02-28 00:23:57 -0500

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app">
-    <div class="title">
+    <router-link to="/" class="title">
       <span class="gold">Bitcoin</span> Smart Contracts
-    </div>
+    </router-link>
 
     <div class="view">
       <router-view></router-view>
@@ -14,7 +14,7 @@
 
     <div class="footer">
       <a href="https://github.com/ildarmgt/bitcoin-web-contracts" class="clean">
-        Source Code
+        {{'< '}}Source Code{{' >'}}
       </a>
       <div class="clean">ildarmgt@gmail.com</div>
     </div>
@@ -29,11 +29,26 @@
 </script>
 
 <style scoped>
+  .title {
+    display: inline-block;
+    font-size: 4vh;
+    /* color:rgb(17, 17, 51); */
+    color:rgb(255, 255, 255);
+    background-color: rgb(17, 17, 51);
+    margin: 1vh 1vh;
+    padding: 1.5vh 10vw;
+    text-decoration: none;
+  }
+
   .footer {
     margin: 3vh 0 0 0;
     font-size: 1.5vh;
   }
   .gold {
     color:rgb(255, 218, 51);
+  }
+
+  div {
+    cursor: default;
   }
 </style>
