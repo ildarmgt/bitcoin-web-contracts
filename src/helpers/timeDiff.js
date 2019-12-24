c13f3d895dcebb31361193c3cf74cd3ed257fc4b
ildarmgt <ildarmgt@gmail.com>
2019-12-24 15:46:35 -0500

    time diff fixed

--- src/helpers/timeDiff.js
+++ src/helpers/timeDiff.js
@@ -1,7 +1,8 @@
 export default function timeDiff (inTimeStamp, daysLocked) {
   const msLocked = daysLocked * 24 * 60 * 60 * 1000;
-  let diff = Date.now() - (new Date(inTimeStamp + msLocked));
+  let diff = Date.now() - (inTimeStamp + msLocked);
   const unlocked = (diff > 0);
+  diff = Math.abs(diff);
   const d = Math.floor(diff / (1000 * 60 * 60 * 24));
   diff -= d * (1000 * 60 * 60 * 24); // remove days
   const h = Math.floor(diff / (1000 * 60 * 60)) % 24;
@@ -9,8 +10,8 @@ export default function timeDiff (inTimeStamp, daysLocked) {
   const m = Math.floor(diff / (1000 * 60)) % 60;
   const word = unlocked ? 'ago' : 'left';
   return {
-    dhm: `${Math.abs(d)} days ${h} hours ${m} min ${word}`,
-    dh: `${Math.abs(d)} days ${h} hours ${word}`,
+    dhm: `${d} days ${h} hours ${m} min ${word}`,
+    dh: `${d} days ${h} hours ${word}`,
     unlocked: unlocked
   };
 }
