b683779cd175359c78432560e4f790c90147a14b
ildarmgt <ildarmgt@gmail.com>
2020-01-07 22:19:41 -0500

    typo

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
   // relative pathing, allows use from file system, must use hash router
   publicPath: '',
 
-  // npm run build puts build into /docs/ fodler for github pages
+  // npm run build puts build into /docs/ folder for github pages
   outputDir: 'docs'
 };
